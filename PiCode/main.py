5a4efe1ba57268dab36745e77aeb424b58c90839
clay53 <clayton.hickey@yahoo.com>
2018-06-25 19:15:21 +0000

    basic functionality

---
+++ PiCode/main.py
@@ -0,0 +1,26 @@
+import os
+from time import sleep
+import RPi.GPIO as GPIO
+
+indexPin = 17
+indexPin2 = 27
+
+video1 = "Welcome.mov"
+video2 = "RedSnow.mp4"
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(indexPin, GPIO.IN)
+GPIO.setup(indexPin2, GPIO.IN)
+
+def my_callback(channel):
+    print("GPIO" + str(channel) + " changed to " + str(GPIO.input(channel)))
+    if (channel == indexPin):
+        os.system("omxplayer -b " + video1)
+    else:
+        os.system("omxplayer -b " + video2)
+
+GPIO.add_event_detect(indexPin, GPIO.FALLING, callback=my_callback)
+GPIO.add_event_detect(indexPin2, GPIO.FALLING, callback=my_callback)
+
+while True:
+    sleep(100)
